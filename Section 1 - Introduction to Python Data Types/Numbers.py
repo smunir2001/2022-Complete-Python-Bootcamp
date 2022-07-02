1e4e1bc7eaaa51f44162310176723789517c3a50
Sami Munir <smunir2001@gmail.com>
2022-07-02 15:33:52 -0400

    added Numbers.py

---
+++ Section 1 - Introduction to Python Data Types/Numbers.py
@@ -0,0 +1,31 @@
+# smunir2001@gmail.com | July 2, 2022 | Numbers.py
+
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(3 % 2)
+print(3 ** 2)
+print((2 + 10) * (10 + 3))
+
+# Variables
+my_dogs = 2
+print("\ntype(my_dogs): ", type(my_dogs))
+print("my_dogs = ", my_dogs)
+my_dogs = ["Sammy", "Frankie"]
+print("now type(my_dogs): ", type(my_dogs))
+print("now my_dogs = ", my_dogs)
+
+a = 5
+print("\ntype(a): ", type(a))
+print("a = ", a)
+a = a + a
+print("now type(a): ", type(a))
+print("now a = ", a)
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print("\nIncome: $", my_income)
+print("Tax Rate: $", tax_rate)
+print("My taxes: $", my_taxes)
