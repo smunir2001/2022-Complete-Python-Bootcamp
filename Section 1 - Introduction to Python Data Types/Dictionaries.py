2f37a7fee048d52cf5d26a978426f4839eb0abc1
Sami Munir <smunir2001@gmail.com>
2022-07-04 01:35:48 -0400

    added Dictionaries.py

---
+++ Section 1 - Introduction to Python Data Types/Dictionaries.py
@@ -0,0 +1,10 @@
+# smunir2001@gmail.com | July 3, 2022 | Dictionaries.py
+
+my_dict = {'key1' : 'value1', 'key2' : 'value2'}
+print(my_dict)
+
+prices_lookup = {'apples' : 2.99, 'oranges' : 1.99, 'milk' : 5.80}
+print(prices_lookup)
+
+d = {'k1' : 123, 'k2' : [0, 1, 2], 'k3' : {'insideKey' : 100}}
+print(d)
