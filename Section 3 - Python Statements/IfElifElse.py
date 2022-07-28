0bf36fa0fa9e589e708e03664bf43cc791782833
Sami Munir <smunir2001@gmail.com>
2022-07-28 14:05:14 +0100

    added IfElifElse.py

---
+++ Section 3 - Python Statements/IfElifElse.py
@@ -0,0 +1,33 @@
+if True:
+    print('It is true!\n')
+
+if (3 >= 2):
+    print('3 is greater than or equal to 2.\n')
+
+hungry = True
+if hungry:
+    print('FEED ME bissshhh!\n')
+
+hungry = False
+if hungry:
+    print('FEED ME bisssh!\n')
+else:
+    print('I aint hungry goodbye...\n')
+
+# multiple if, elif, else branches
+loc = 'Bank'
+if loc == 'Auto Shop':
+    print('Cars are cool :)\n')
+elif loc == 'Bank':
+    print('Money buys happiness hehe\n')
+elif loc == 'Store':
+    print('What kind of store are we really talking about ;) ???\n')
+else: print('I do not know much :(\n')
+
+name = 'Sammy'
+if (name == 'Frankie'):
+    print('Hello Frankie...\n')
+elif name == 'Sammy':
+    print('Wassup Sambreezy :$\n')
+else:
+    print('What is your name???\n') 
